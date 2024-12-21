7d72dba3f9f20602513aa55277753296f5313c8d
denis.malabuyok <denis.malabuyok@gmail.com>
2024-12-21 22:54:55 +0300

    2.0 deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import {
+	addNewCard,
+	editUserData,
+	getInitialsCards,
+	getUserData,
+} from './components/api.js'
 import { createCard, deleteCard, setLikeToCard } from './components/card.js'
-import { initialCards } from './components/cards.js'
 import { closeModal, openModal } from './components/modal.js'
+import { clearValidation, enableValidation } from './components/validation.js'
 import './pages/index.css'
 
 const placesContainer = document.querySelector('.places__list')
@@ -10,6 +16,7 @@ const editProfileForm = document.querySelector(
 const addCardForm = document.querySelector('.popup__form[name="new-place"]')
 const profileTitle = document.querySelector('.profile__title')
 const profileDescription = document.querySelector('.profile__description')
+const profileImage = document.querySelector('.profile__image')
 const popupInputTypeName = editProfileForm.querySelector(
 	'.popup__input_type_name'
 )
@@ -26,12 +33,23 @@ const closePopupButtons = document.querySelectorAll('.popup__close')
 const popupImage = document.querySelector('.popup_type_image')
 const popupImageElement = popupImage.querySelector('.popup__image')
 const popupCaptionElement = popupImage.querySelector('.popup__caption')
+const validationConfig = {
+	formSelector: '.popup__form',
+	inputSelector: '.popup__input',
+	submitButtonSelector: '.popup__button',
+	inactiveButtonClass: 'popup__button_disabled',
+	inputErrorClass: 'popup__input_type_error',
+	errorClass: 'popup__error_visible',
+}
 profileEditButton.addEventListener('click', () => {
 	popupInputTypeName.value = profileTitle.textContent
 	popupInputTypeDescription.value = profileDescription.textContent
+	clearValidation(editProfileForm, validationConfig)
 	openModal(popupTypeEdit)
 })
 profileAddButton.addEventListener('click', () => {
+	addCardForm.reset()
+	clearValidation(addCardForm, validationConfig)
 	openModal(newCardPopup)
 })
 closePopupButtons.forEach(button => {
@@ -46,6 +64,7 @@ function handleEditFormSubmit(evt) {
 	evt.preventDefault()
 	const nameValue = popupInputTypeName.value
 	const jobValue = popupInputTypeDescription.value
+	editUserData(nameValue, jobValue)
 	profileTitle.textContent = nameValue
 	profileDescription.textContent = jobValue
 	closeModal(popupTypeEdit)
@@ -60,17 +79,21 @@ function addCardSubmit(evt) {
 	evt.preventDefault()
 	const cardName = cardNameInput.value
 	const imageUrl = imageUrlInput.value
+	addNewCard(cardName, imageUrl).then(card => {
 	const newCard = createCard(
+		card._id,
 		cardName,
 		imageUrl,
 		deleteCard,
+		card.likes,
 		setLikeToCard,
-		openImagePopup
+		openImagePopup,
+		card.owner._id
 	)
 	addCard(newCard, true)
 	addCardForm.reset()
 	closeModal(newCardPopup)
-}
+})
 function addCard(cardElement, toStart) {
 	if (toStart === true) {
 		placesContainer.prepend(cardElement)
@@ -78,15 +101,28 @@ function addCard(cardElement, toStart) {
 		placesContainer.append(cardElement)
 	}
 }
-initialCards.forEach(card => {
-	const newCard = createCard(
-		card.name,
-		card.link,
-		deleteCard,
-		setLikeToCard,
-		openImagePopup
-	)
-	addCard(newCard)
-})
+Promise.all([getUserData(), getInitialsCards()])
+	.then(([userData, initialCards]) => {
+		currentUserId = userData._id
+		profileTitle.textContent = userData.name
+		profileDescription.textContent = userData.about
+		profileImage.src = `${userData.avatar}`
+		console.log(initialCards)
+		initialCards.forEach(card => {
+			const newCard = createCard(
+				card._id,
+				card.name,
+				card.link,
+				deleteCard,
+				card.likes,
+				setLikeToCard,
+				openImagePopup,
+				card.owner._id
+			)
+			addCard(newCard)
+		})
+	})
+	.catch(error => console.log(error))
 editProfileForm.addEventListener('submit', handleEditFormSubmit)
 addCardForm.addEventListener('submit', addCardSubmit)
+enableValidation(validationConfig)
